3ac1a0774c40196f53b35f6b57089c0daae462b3
Balagopal K.V. <balagopalkv@gmail.com>
2025-01-06 21:43:45 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Guest from "./components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+const HomePage = async () => {
+  const user = await currentUser();
+  
+  if (!user){
+    return <Guest />
+  }
+  
+  return ( 
+  <main>
+    <h1>Welcome, {user.firstName}</h1>
+  </main> );
+}
+ 
+export default HomePage;
